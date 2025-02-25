9ddd2cdb26344b536ada2eaa9f861ac0a3ea4a59
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-02-25 23:26:01 +0300

    made safaricard shorter by using props or something

--- src/Components/SafariCard.js
+++ src/Components/SafariCard.js
@@ -5,11 +5,12 @@ import React, { useState } from "react";
 const SafariCard = ({ image, title, description}) => {
     return (
         <div className="safari-card">
-            <img src="https://maxm-imggenurl.web.val.run/maasai-mara-safari-landscape-with-lions-and-acacia-trees" alt="Maasai Mara NationalReserve" className="destination-image"/>
+            <img src={image} alt={title} className="destination-image"/>
             <div className="destination-content">
-                <h3>Maasai Mara National Reserve</h3>
-                <p>Famous for the Great Migration and abundant wildlife</p>
+                <h3>{title}</h3>
+                <p>{description}</p>
             </div>
         </div>
     );
 }
+export default SafariCard;
